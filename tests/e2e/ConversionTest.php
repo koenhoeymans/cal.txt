f294dd31d985ada3746b30119c57097175ec8fce
koenhoeymans <koenhoeymans@gmail.com>
2022-02-14 14:23:07 +0100

    fixes

--- tests/e2e/ConversionTest.php
+++ tests/e2e/ConversionTest.php
@@ -10,8 +10,8 @@ class ConversionTest extends \PHPUnit\Framework\TestCase
 
     public function setup(): void
     {
-        $this->inputFile = __DIR_ . DIRECTORY_SEPARATOR . 'sample_calendar.txt';
-        $this->outputFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'sampe_calendar.ics';
+        $this->inputFile = __DIR__ . DIRECTORY_SEPARATOR . 'sample_calendar.txt';
+        $this->outputFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'sample_calendar.ics';
         if(file_exists($this->outputFile)) {
             unlink($this->outputFile);
         }
@@ -29,7 +29,7 @@ class ConversionTest extends \PHPUnit\Framework\TestCase
      */
     public function it_converts_caltxt_to_ics_file(): void
     {
-        $expected_content = file_get_content($this->outputFile);
+        $expected_content = file_get_contents($this->outputFile);
         $actual_content = $this->converter->convert($this->inputFile);
 
         $this->assertTrue($expected_content, $actual_content);
